ad7c15b3d057fc3c9df776fc245fbc7815ae7482
TomInJpn <66042540+TomInJpn@users.noreply.github.com>
2020-11-04 15:41:46 +0900

    Update onload.js
    実行タイミング見直し

--- js/onload.js
+++ js/onload.js
@@ -1,16 +1,12 @@
-$(function(){
+function portfolio_onload(){
+  $(".loading").css("display","none");
+  $("header nav .top").css("animation","top_shrink 3s");
+}
 
-  function portfolio_onload(){
-    $(".loading").css("display","none");
-    $("header nav .top").css("animation","top_shrink 3s");
-  }
-
-  $(window).on('load', function(){
-    portfolio_onload();
-  });
-
-  setTimeout(function(){
-    portfolio_onload();
-  },1000);
-
+$(window).on('load', function(){
+  portfolio_onload();
 });
+
+setTimeout(function(){
+  portfolio_onload();
+},3000);
